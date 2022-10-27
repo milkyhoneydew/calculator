0fc4c8788ccd28170339ba344714253d79df050c
Tanya Prado <tanyaprado@gmail.com>
2022-10-26 19:43:43 -0700

    add alerts for number functions

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+const number1 = parseInt(prompt("Enter a number: "));
+const number2 = parseInt(prompt("Enter another number: "));
+const num1String = number1.toString();
+const num2String = number2.toString();
+
+window.alert(num1String + "+" + num2String + "="  + add(number1, number2));
+
+window.alert(num1String + "-" + num2String + "="  + subtract(number1, number2));
+
+window.alert(num1String + "*" + num2String + "="  + multiply(number1, number2));
+
+window.alert(num1String + "/" + num2String + "="  + division(number1, number2));
